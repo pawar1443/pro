0539faacfd9656e3e31998a94b5012e87232423d
pawar1443 <pawarbirai@gmail.com>
2025-09-13 23:51:22 +0530

    added polymorphism file

---
+++ polymorphism.java
@@ -0,0 +1,28 @@
+package pro;
+class prey{
+    void hunt(){
+        System.out.println("the prey hunts different animals");
+    }
+}
+class hawk extends prey{
+    @Override
+    void hunt(){
+        System.out.println("hawk also hunts and eats its pray");
+    }
+}
+class lion extends prey{
+    @Override
+    void hunt(){
+        System.out.println("lion also hunts and eats its pray");
+    }
+}
+public class polymorphism {
+    public static void main(String args[]){
+        prey p;
+
+        p = new hawk();
+        p.hunt();
+        p = new lion();
+        p.hunt();
+    }
+}
