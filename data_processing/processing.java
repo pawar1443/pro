e0bccb7b4939cf2369e25eac5c75f886b563729d
pawar1443 <pawarbirai@gmail.com>
2025-09-19 00:46:56 +0530

    the first skip line code was wrong , this will skip the first row

--- data_processing/processing.java
+++ data_processing/processing.java
@@ -1,18 +1,15 @@
 package pro.data_processing;
-import java.util.*;
-import java.io.*;
-import java.util.stream.Stream;
+import java.io.IOException;
 import java.nio.file.*;
+import java.util.stream.Stream;
 
 public class processing {
-    public static void main(String args[]){
-        try{
-            Stream<String> lines = Files.lines(Paths.get("pro/data_processing/data.csv"));
-            lines.skip(1);
-            lines.forEach(System.out::println);
-            lines.close();
-        }catch(IOException e){
+    public static void main(String[] args) {
+        try (Stream<String> lines = Files.lines(Paths.get("pro/data_processing/data.csv"))) {
+            lines.skip(1) // skip header
+                 .forEach(System.out::println); // print remaining lines
+        } catch (IOException e) {
             e.printStackTrace();
-        } 
+        }
     }
 }
