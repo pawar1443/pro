9ecc6a0c2dd9a4e77dc4cd7b73a11a1bb67c33fb
pawar1443 <pawarbirai@gmail.com>
2025-09-20 01:51:00 +0530

    the polymorphism coe was giving error

--- poly.java
+++ poly.java
@@ -1,28 +1,31 @@
 package pro;
-class prey{
-    void hunt(){
-        System.out.println("the prey hunts different animals");
-    }
-}
-class hawk extends prey{
-    @Override
-    void hunt(){
-        System.out.println("hawk also hunts and eats its pray");
-    }
-}
-class lion extends prey{
-    @Override
-    void hunt(){
-        System.out.println("lion also hunts and eats its pray");
-    }
-}
-public class poly {
-    public static void main(String args[]){
-        prey p;
 
-        p = new hawk();
-        p.hunt();
-        p = new lion();
-        p.hunt();
+public class poly {
+    static class prey {
+        void hunt() {
+            System.out.println("the prey hunts different animals");
+        }
+    }
+
+    static class hawk extends prey {
+        @Override
+        void hunt() {
+            System.out.println("hawk also hunts and eats its prey");
+        }
+    }
+
+    static class lion extends prey {
+        @Override
+        void hunt() {
+            System.out.println("lion also hunts and eats its prey");
+        }
+    }
+
+    public static void main(String[] args) {
+        prey p1 = new hawk();
+        p1.hunt();
+
+        prey p2 = new lion();
+        p2.hunt();
     }
 }
