25649aa8101529260c0b3d8c090faa5078e5294f
pawar1443 <pawarbirai@gmail.com>
2025-09-21 11:30:02 +0530

    this is pending

---
+++ data_processing/newProcessing.java
@@ -0,0 +1,10 @@
+package pro.data_processing;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+
+public class newProcessing {
+    public static void main(String[] args) throws FileNotFoundException {
+        FileReader fileReader = new FileReader("C:\\Users\\pawar\\IdeaProjects\\pro\\data_processing\\data.csv");
+    }
+}
